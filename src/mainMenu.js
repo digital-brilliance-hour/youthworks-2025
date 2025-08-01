852067be93b74ad29ffe2db9e629ab047bf2c2c0
digitalbrilliancehour <48660450+digitalbrilliancehour@users.noreply.github.com>
2025-08-01 11:45:47 -0400

    Update mainMenu.js

--- src/mainMenu.js
+++ src/mainMenu.js
@@ -20,7 +20,7 @@ BasicGame.MainMenu.prototype = {
 
     this.titlepage.height = 600;
 
-    this.loadingText = this.add.text(this.game.width / 2, this.game.height / 2 + 80, "Press Z or tap/click game to start", { font: "20px monospace", fill: "#fff" });
+    this.loadingText = this.add.text(this.game.width / 2, this.game.height / 2 + 80, "Press Z or tap/click game to start", { font: "20px monospace", fill: "#fff", stroke: "black", strokeThickness: 2 });
     this.loadingText.anchor.setTo(0.5, 0.5);
     this.add.text(this.game.width / 2, this.game.height - 90, "image assets Copyright (c) 2002 Ari Feldman", { font: "12px monospace", fill: "#fff", align: "center"}).anchor.setTo(0.5, 0.5);
     this.add.text(this.game.width / 2, this.game.height - 75, "sound assets Copyright (c) 2012 - 2013 Devin Watson", { font: "12px monospace", fill: "#fff", align: "center"}).anchor.setTo(0.5, 0.5);
